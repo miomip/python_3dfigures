a5d6df2f6abe68081f0649906bec0549bce12e48
Silent Sky(Phillip Johnerud) <88206755+miomip@users.noreply.github.com>
2024-04-27 19:29:06 +0200

    Add files via upload

---
+++ house.py
@@ -0,0 +1,47 @@
+from math import sqrt
+import turtle as tl
+from lib.Colors import Colors
+
+def small_house():
+    house_length = 50
+    tl.right(90)
+    tl.fd(house_length/2)
+    tl.left(90)
+    for i in range(1, 4):
+        tl.fd(house_length)
+        tl.left(90)
+    tl.fd(house_length/2)
+    tl.right(180)
+    tl.fd(house_length/2)
+    tl.right(30)
+    math_for_roof = sqrt((house_length*0.87)**2+(house_length/2)**2)
+    tl.fd(math_for_roof)
+    tl.right(120)
+    tl.fd(math_for_roof)
+    tl.hideturtle()
+    tl.exitonclick()
+
+def big_house():
+    return 0
+
+def house():
+    types = {
+        'small' : small_house,
+        'big': big_house
+    }
+    inp = input("what size do you want?")
+    if types.get(inp.lower()) == None:
+        return print(Colors.bold
+                     +Colors.red
+                     +"Exception:"
+                     +Colors.gray
+                     +" IllegalArgumentException"
+                     +"\n"
+                     +Colors.red
+                     +"Error:"
+                     +Colors.gray 
+                     +" -1" 
+                     +Colors.resetColor
+                     )
+    else:
+        return types[inp.lower()]()
---
+++ sideeye.py
@@ -0,0 +1,24 @@
+from turtle import * # type: ignore
+
+
+def Sideeye():
+    screensize(500,500,bg="black")
+    begin_fill()
+    circle(120, color("Yellow"))
+    end_fill()
+    lt(90)
+    penup()
+    fd(100)
+    rt(90)
+    fd(50)
+    pendown()
+    begin_fill()
+    circle(35, color("black"))
+    end_fill()
+    bk(200)
+    begin_fill()
+    circle(35, color("black"))
+    end_fill()
+    screensize(500, 500, bg="black")
+    hideturtle()
+    exitonclick()
