fceaaf71b71a8c349092d4e086ef40ee9c509be4
Silent Sky(Phillip Johnerud) <88206755+miomip@users.noreply.github.com>
2024-05-05 20:17:29 +0200

    shit

--- Experimental/main.py
+++ Experimental/main.py
@@ -1,10 +1,13 @@
 import pygame
 
 pygame.init()
+display = pygame.display
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 dt = 0
+
+display.set_caption("Shape")
 
 player_pos = pygame.Vector2(screen.get_width()/2, screen.get_height()/2)
 
@@ -13,24 +16,11 @@ def projection_3d():
 
 while running:
     for event in pygame.event.get():
-        if event == pygame.QUIT:
+        if event.type == pygame.QUIT:
             running = False
     
-    screen.fill("purple")
+    screen.fill("lightblue")
 
-    pygame.draw.circle(screen, "blue", player_pos, 20)
-
-    keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
-        
     pygame.display.flip()
 
     dt = clock.tick(120) / 1000
