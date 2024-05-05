d926f14a84b86f2f71112a8567a9395cc194e477
Silent Sky(Phillip Johnerud) <88206755+miomip@users.noreply.github.com>
2024-05-05 20:30:58 +0200

    Changed some things

--- Experimental/main.py
+++ Experimental/main.py
@@ -1,27 +1,94 @@
 import pygame
+from math import cos, sin, pi
+from numpy import matrix
+from time import sleep
+from random import randint
 
 pygame.init()
 display = pygame.display
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
-running = True
+done = False
 dt = 0
+
+width = 500
+height = 500
+white = (255, 255, 255)
+
 
 display.set_caption("Shape")
 
 player_pos = pygame.Vector2(screen.get_width()/2, screen.get_height()/2)
 
-def projection_3d():
-    return 0
+spoints = 50
+points_3d = (
+    ( spoints,  spoints,  spoints),
+    (-spoints,  spoints,  spoints),
+    ( spoints, -spoints,  spoints),
+    ( spoints,  spoints, -spoints),
+    (-spoints,  spoints, -spoints),
+    ( spoints, -spoints, -spoints),
+    (-spoints, -spoints, -spoints),
+    (-spoints, -spoints,  spoints),
+)
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+rotation = [0,0,0]
+
+def generate_x(x):
+    return matrix([
+        [1, 0, 0],
+        [0, cos(x), -sin(x)],
+        [0, sin(x), cos(x)]
+    ])
+
+def generate_y(x):
+    return matrix([
+        [cos(x), 0, -sin(x)],
+        [0, 1, 0],
+        [sin(x), 0, cos(x)]
+    ])
+
+def generate_z(x):
+    return matrix([
+        [cos(x), -sin(x), 0],
+        [sin(x), cos(x), 0],
+        [0, 0, 1]
+    ])
+
+
+
+
+while not done:
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            done = True
+            break
     
-    screen.fill("lightblue")
+    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, 0, width, height))
 
-    pygame.display.flip()
+    render_points = []
+
+    for p in points_3d:
+
+        m = matrix([
+        [p[0]],
+        [p[1]],
+        [p[2]]
+        ])
+
+        for method, angle in zip((generate_x, generate_y, generate_z), rotation):
+            m = method(angle) * m
+
+        x, y, z = map(lambda x: int(width/2 - x), (m[0,0], m[1,0], m[2,0]))
+
+        render_points.append((x, y))
+
+    for p1 in range(len(render_points) - 1):
+        for p2 in render_points[p1 + 1:]:
+            pygame.draw.line(screen, white, render_points[p1], p2)
+
+    rotation[0] += pi / randint(100, 200)
+    rotation[1] += pi / randint(100, 200)
 
     dt = clock.tick(120) / 1000
 
